d623299a202b4d5e73cfcf4c92adbe8f7041127a
Jeong yong hoon <jyh.dev@gmail.com>
2022-04-03 15:51:51 +0900

    프로젝트 레이아웃 설정

---
+++ javascript-study/src/AppStyle.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AppPageLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+`
