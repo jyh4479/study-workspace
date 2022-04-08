29830f4acaa274c8971bc8b6fb1d34ca6859823b
Jeong yong hoon <jyh.dev@gmail.com>
2022-04-08 17:52:40 +0900

    Re - Rendering
    제일 기본인 렌더링에 관련된 테스트...
    아직 많이 부족하다..

---
+++ javascript-study/src/container/RenderingContainer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {RenderingContainerLayout} from "./style";
+
+// props가 바뀌어서 렌더링된다고해도 원래 가지고있는 state가 바뀌는게 아님
+const RenderingContainer = props => {
+    return (
+        <RenderingContainerLayout>
+
+        </RenderingContainerLayout>
+    )
+}
+
+export default RenderingContainer;
---
+++ javascript-study/src/container/RenderingContainer/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+import {Center} from "../../common/display";
+
+export const RenderingContainerLayout = styled(Center())`
+`
