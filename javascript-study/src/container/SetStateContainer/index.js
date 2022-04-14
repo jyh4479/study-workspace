1c2b0f8d2e510390824d6ddc6bd6bb17781e9478
Jeong yong hoon <jyh.dev@gmail.com>
2022-04-14 19:21:20 +0900

    SETSTATE LAYOUT
    setState의 동작을 공부하고 테스트하기 위한 컨테이너

---
+++ javascript-study/src/container/SetStateContainer/index.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+
+const SetStateContainer = props => {
+
+    const [count, setCount] = useState(0);
+    const [confirm, setConfirm] = useState(false);
+    
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SetStateContainer;
---
+++ javascript-study/src/container/SetStateContainer/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import {Center} from "../../common/display";
+
+export const SetStateContainerLayout = styled(Center)`
+
+`
