1258b66ae3b53b19e217a4171e4c9930498c1d19
Jeong yong hoon <jyh.dev@gmail.com>
2022-05-06 22:33:48 +0900

    CUSTOM TABLE
    Custom tui grid

--- javascript-study/src/container/TableTestContainer/TuiDataTableContainer/index.js
+++ javascript-study/src/container/TableTestContainer/TuiDataTableContainer/index.js
@@ -1,50 +1,60 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {columns, data} from "./data";
 import "tui-grid/dist/tui-grid.css";
 import Grid from "@toast-ui/react-grid";
 
-
 //https://github.com/nhn/tui.grid/blob/master/packages/toast-ui.grid/docs/ko/custom-renderer.md#%EB%94%94%ED%8F%B4%ED%8A%B8-%EB%A0%8C%EB%8D%94%EB%9F%AC-%EC%8A%A4%ED%83%80%EC%9D%BC%EB%A7%81
 // https://github.com/nhn/tui.grid/blob/master/packages/toast-ui.grid/docs/ko/sort.md
 const TuiDataTableContainer = () => {
-    const MyComponent = () => (
-        <Grid
-            data={data}
-            columns={columns}
-            rowHeight={25}
-            bodyHeight={200}
-            heightResizable={true}
-            rowHeaders={['rowNum']}
+    const tableRef = React.createRef();
 
-            pageOptions={{
-                useClient: true,
-                page: 1,
-                perPage: 10,
-            }}
-        />
-    );
+    // const removeAllEventListener = (el, type) => {
+    //
+    // }
+
+    const addCustomEventListener = (el, type, func) => {
+        el.addEventListener(type, func)
+    }
+
+    useEffect(() => {
+        const tableHeaders = tableRef.current.querySelectorAll(".tui-grid-btn-sorting");
+
+        tableHeaders.forEach(tableHeader => {
+
+            // removeCustomEventListener(tableHeader, 'click');
+
+            addCustomEventListener(tableHeader, 'click', () => {
+                if (tableHeader.classList.contains("tui-grid-btn-sorting-up")) {
+                    console.log("up");
+                    tableHeader.style.background = `green`
+                } else if (tableHeader.classList.contains("tui-grid-btn-sorting-down")) {
+                    console.log("down");
+                    tableHeader.style.background = `blue`
+                } else {
+                    console.log("test");
+                    tableHeader.style.background = `red`
+                }
+            });
+        })
+
+    }, [tableRef])
+
+    const MyComponent = () => (<div ref={tableRef}>
+            <Grid
+                data={data}
+                columns={columns}
+                rowHeight={25}
+                bodyHeight={200}
+                heightResizable={true}
+                rowHeaders={['rowNum']}
+
+                pageOptions={{
+                    useClient: true, page: 1, perPage: 10,
+                }}
+            />
+        </div>);
 
     return MyComponent();
-
-
-    // return (
-    //     <TuiTableContainerLayout>
-    //         <TestGridBack>
-    //             <Grid
-    //                 data={data}
-    //                 columns={columns}
-    //                 // heightResizable={true}
-    //                 rowHeaders={['rowNum']}
-    //                 sortable={true}
-    //                 pageOptions={{
-    //                     useClient: true,
-    //                     page: 1,
-    //                     perPage: 10,
-    //                 }}
-    //             />
-    //         </TestGridBack>
-    //     </TuiTableContainerLayout>
-    // )
 }
 
 export default TuiDataTableContainer;
