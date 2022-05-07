70d1c16e9557bcc4da99cb127bb309d136c08535
Jeong yong hoon <jyh.dev@gmail.com>
2022-05-07 13:52:59 +0900

    UPDATE CUSTOM GRID
    Grid Event 중복 등록 방지 테스트 성공

--- javascript-study/src/container/TableTestContainer/TuiDataTableContainer/index.js
+++ javascript-study/src/container/TableTestContainer/TuiDataTableContainer/index.js
@@ -2,18 +2,32 @@ import React, {useEffect} from "react";
 import {columns, data} from "./data";
 import "tui-grid/dist/tui-grid.css";
 import Grid from "@toast-ui/react-grid";
+import {isEmpty} from "lodash";
 
 //https://github.com/nhn/tui.grid/blob/master/packages/toast-ui.grid/docs/ko/custom-renderer.md#%EB%94%94%ED%8F%B4%ED%8A%B8-%EB%A0%8C%EB%8D%94%EB%9F%AC-%EC%8A%A4%ED%83%80%EC%9D%BC%EB%A7%81
 // https://github.com/nhn/tui.grid/blob/master/packages/toast-ui.grid/docs/ko/sort.md
 const TuiDataTableContainer = () => {
     const tableRef = React.createRef();
-
-    // const removeAllEventListener = (el, type) => {
-    //
-    // }
+    const currentEvent = {};
 
     const addCustomEventListener = (el, type, func) => {
         el.addEventListener(type, func)
+
+        if (isEmpty(currentEvent[type])) {
+            currentEvent[type] = [];
+        }
+        currentEvent[type].push([el, func]);
+    }
+
+    const removeCustomEventListener = type => {
+        if (!isEmpty(currentEvent[type])) {
+            currentEvent[type].forEach(componentInfo => {
+                const component = componentInfo[0];
+                const event = componentInfo[1];
+
+                component.removeEventListener(type, event);
+            })
+        }
     }
 
     useEffect(() => {
@@ -36,23 +50,23 @@ const TuiDataTableContainer = () => {
                 }
             });
         })
-
+        return () => removeCustomEventListener('click');
     }, [tableRef])
 
     const MyComponent = () => (<div ref={tableRef}>
-            <Grid
-                data={data}
-                columns={columns}
-                rowHeight={25}
-                bodyHeight={200}
-                heightResizable={true}
-                rowHeaders={['rowNum']}
+        <Grid
+            data={data}
+            columns={columns}
+            rowHeight={25}
+            bodyHeight={200}
+            heightResizable={true}
+            rowHeaders={['rowNum']}
 
-                pageOptions={{
-                    useClient: true, page: 1, perPage: 10,
-                }}
-            />
-        </div>);
+            pageOptions={{
+                useClient: true, page: 1, perPage: 10,
+            }}
+        />
+    </div>);
 
     return MyComponent();
 }
