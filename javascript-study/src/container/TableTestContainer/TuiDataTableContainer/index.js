7971e1de7c5d29359c28f14b0aa24f56b477b810
Jeong yong hoon <jyh.dev@gmail.com>
2022-05-09 22:11:06 +0900

    ADD INITIAL LOGIC
    useRef 사용시 스타일 저장되는 현상 공부하기

--- javascript-study/src/container/TableTestContainer/TuiDataTableContainer/index.js
+++ javascript-study/src/container/TableTestContainer/TuiDataTableContainer/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import {columns, data} from "./data";
 import "tui-grid/dist/tui-grid.css";
 import Grid from "@toast-ui/react-grid";
@@ -7,7 +7,7 @@ import {isEmpty} from "lodash";
 //https://github.com/nhn/tui.grid/blob/master/packages/toast-ui.grid/docs/ko/custom-renderer.md#%EB%94%94%ED%8F%B4%ED%8A%B8-%EB%A0%8C%EB%8D%94%EB%9F%AC-%EC%8A%A4%ED%83%80%EC%9D%BC%EB%A7%81
 // https://github.com/nhn/tui.grid/blob/master/packages/toast-ui.grid/docs/ko/sort.md
 const TuiDataTableContainer = () => {
-    const tableRef = React.createRef();
+    const tableRef = useRef(null);
     const currentEvent = {};
 
     const addCustomEventListener = (el, type, func) => {
@@ -25,6 +25,8 @@ const TuiDataTableContainer = () => {
                 const component = componentInfo[0];
                 const event = componentInfo[1];
 
+                // backGround 초기화 해주는 로직 --> useRef 사용시 reRendering 해도 스타일이 남는 현상 공부
+                component.style.background = `black`
                 component.removeEventListener(type, event);
             })
         }
