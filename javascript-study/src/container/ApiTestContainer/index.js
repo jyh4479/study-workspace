5605d46654c028352ca4da5393619339a62d9dc4
jyh4479 <jyh.dev@gmail.com>
2022-09-29 22:08:42 +0900

    TEST ASYNC AWAIT, PROMISE

--- javascript-study/src/container/ApiTestContainer/index.js
+++ javascript-study/src/container/ApiTestContainer/index.js
@@ -9,25 +9,52 @@ const ApiTestContainer = (props) => {
 
     const getAPI = async () => {
         console.log("First API Call0");
+
         const result = await axios.get(`https://jsonplaceholder.typicode.com/todos/${dataKey}`);
+
         console.log("First API Call1");
-        setFirstApiData(prev => {
-            console.log("First API Call2")
-            return result.data?.title;
-        });
+
+        const result2 = await axios.get(`https://jsonplaceholder.typicode.com/todos/${dataKey}`);
+
+        console.log("First API Call2");
+
+        const result3 = await axios.get(`https://jsonplaceholder.typicode.com/todos/${dataKey}`);
+
         console.log("First API Call3");
+
+
+        // setFirstApiData(prev => {
+        //     console.log("First API Call3")
+        //     return result.data?.title;
+        // });
+        //
+        // console.log("First API Call4");
     }
 
     const getAPI2 = () => {
         console.log("Second API Call0");
+
         axios.get(`https://jsonplaceholder.typicode.com/todos/${dataKey}`).then(result => {
             console.log("Second API Call1");
-            setSecondApiData(prev => {
-                console.log("Second API Call2");
-                return result.data?.title;
-            });
         });
-        console.log("Second API Call3");
+
+        axios.get(`https://jsonplaceholder.typicode.com/todos/${dataKey}`).then(result => {
+            console.log("Second API Call2");
+        });
+
+        axios.get(`https://jsonplaceholder.typicode.com/todos/${dataKey}`).then(result => {
+            console.log("Second API Call3");
+        });
+
+        console.log("Second API Call4");
+
+        // axios.get(`https://jsonplaceholder.typicode.com/todos/${dataKey}`).then(result => {
+        //     console.log("Second API Call1");
+        //     setSecondApiData(prev => {
+        //         console.log("Second API Call2");
+        //         return result.data?.title;
+        //     });
+        // });
     }
 
     return (
