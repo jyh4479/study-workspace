b64e52206a12dd5251a43eedc0f8885547541cd8
jyh4479 <jyh.dev@gmail.com>
2022-11-02 14:11:12 +0900

    UPLOAD CIRCLE CAROUSEL LOGIC

--- react-study/src/components/Carousel/indedx.jsx
+++ react-study/src/components/Carousel/indedx.jsx
@@ -6,13 +6,19 @@ const Carousel = (props) => {
 
     const [dataList, setDataList] = useState(carouselData);
 
-    return (
-        <CarouselBox>
-            {dataList.map(data => <CarouselContent><span>{data.value}</span></CarouselContent>)}
-            <CarouselButton className={"left"}>{'<'}</CarouselButton>
-            <CarouselButton className={"right"}>{'>'}</CarouselButton>
-        </CarouselBox>
-    )
+    const onClickLeft = () => {
+        setDataList(prev => [prev[prev.length - 1], ...prev.slice(0, prev.length - 1)])
+    }
+
+    const onClickRight = () => {
+        setDataList(prev => [...prev.slice(1), prev[0]])
+    }
+
+    return (<CarouselBox>
+        {dataList.map(data => <CarouselContent key={data.value}><span>{data.value}</span></CarouselContent>)}
+        <CarouselButton className={"left"} onClick={() => onClickLeft()}>{'<'}</CarouselButton>
+        <CarouselButton className={"right"} onClick={() => onClickRight()}>{'>'}</CarouselButton>
+    </CarouselBox>)
 };
 
 export default Carousel;
