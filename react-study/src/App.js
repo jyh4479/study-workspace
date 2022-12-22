6c610a2294ce138fbfae0a1f15b911a347803622
jyh4479 <jyh.dev@gmail.com>
2022-12-22 23:45:14 +0900

    [ADD]: TEST TOOL
    React profiler test

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,30 +1,28 @@
 import './App.css';
-import BarChart from "./components/BarChart";
-import BarChartRange from "./components/BarChartRange";
-import HorizontalBarChart from "./components/HorizontalBarChart";
-import {MenuList} from "./components/CompoundPattern";
-import A from "./components/RenderingTestComponent/A";
+import {Profiler} from 'react';
+import {ApexBarChart} from "./components/ApexChartJs";
 
 function App() {
+
+    const onRenderCallback = (id, phase, actualDuration, baseDuration, startTime, commitTime, interactions) => {
+        console.log(actualDuration);
+    }
+
     return (
         <div className="App">
-
-            {/*<Carousel/>*/}
-            {/*<BarChart/>*/}
-
-            {/* current testing */}
-            {/*<HorizontalBarChart/>*/}
-            {/*<BarChartRange/>*/}
-
-            {/*<MenuList>*/}
-            {/*    <MenuList.MainMenu/>*/}
-            {/*    <MenuList.SubMenu/>*/}
-            {/*</MenuList>*/}
-
-            <A/>
-
+            <Profiler id="Navigation" onRender={onRenderCallback}>
+                <ApexBarChart/>
+            </Profiler>
         </div>
     );
 }
 
 export default App;
+
+// id, // 방금 커밋된 Profiler 트리의 "id"
+//     phase, // "mount" (트리가 방금 마운트가 된 경우) 혹은 "update"(트리가 리렌더링된 경우)
+//     actualDuration, // 커밋된 업데이트를 렌더링하는데 걸린 시간
+//     baseDuration, // 메모이제이션 없이 하위 트리 전체를 렌더링하는데 걸리는 예상시간
+//     startTime, // React가 언제 해당 업데이트를 렌더링하기 시작했는지
+//     commitTime, // React가 해당 업데이트를 언제 커밋했는지
+//     interactions // 이 업데이트에 해당하는 상호작용들의 집합
---
+++ react-study/src/components/ApexChartJs/index.js
@@ -0,0 +1,1 @@
+export {default as ApexBarChart} from "./BarChart";
