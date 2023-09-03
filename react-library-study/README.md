e298a702e72684c691e1c77b261cf461d24cfa74
jyh4479 <jyh.dev@gmail.com>
2023-09-03 23:09:26 +0900

    fix: directory rename

---
+++ react-library-study/README.md
@@ -0,0 +1,8 @@
+#현 직장에서 React 업무를하며 배우게되는 React관련 지식 정리
+
+- React의 생명 주기 및 상태관리와 관련된 동작 원리
+- Rendering과 관련된 동작 원리와 Performance 관련 학습
+- Directory 구조 및 협업에 편한 Boilerplate, Component 연구
+- 간단 Component 구현
+
+#### 위 3가지 항목에 대한 위주의 공부 (항목은 유동적으로 변경하여 학습)
--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,42 +1,30 @@
-import './App.css';
-import {Chart, ScatterChart} from "./components/ChartJs";
+import React from "react";
+import {AppPageLayout} from "./AppStyle";
+import ApiTestContainer from "./container/ApiTestContainer";
 
-import {
-    BarElement,
-    CategoryScale,
-    Chart as ChartJS,
-    Filler,
-    Legend,
-    LinearScale,
-    LineElement,
-    PointElement,
-    Title,
-    Tooltip
-} from "chart.js";
-import zoomPlugin from "chartjs-plugin-zoom";
-import A from "./components/RenderingTestComponent/A";
-// import boxselectPlugin from "chartjs-plugin-boxselect";
+const App = () => {
+    return (<AppPageLayout>
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, Title, Tooltip, Legend, Filler, zoomPlugin);
+        {/*<TabContainer/>*/}
+        {/*<InputContainer/>*/}
+        {/*<UseStateContainer/>*/}
+        {/*<DateComputeContainer/>*/}
+        {/*<TabListContainer/>*/}
+        {/*<CleanUpContainer/>*/}
+        {/*<ZustandContainer/>*/}
+        {/*<SetStateContainer/>*/}
+        {/*<TryCatchContainer/>*/}
+        {/*<MuiDataTableContainer/>*/}
+        {/*<TuiDataTableContainer/>*/}
+        {/*<WithTestContainer/>*/}
+        {/*<TimeStampTestContainer/>*/}
+        {/*<IndicatorRadioContainer/>*/}
+        {/*<ChartTestContainer/>*/}
+        {/*<ComponentTestContainer/>*/}
+        <ApiTestContainer/>
 
-function App() {
-    return (
-        <div className="App">
-            {/*<ChartLayoutContainer/>*/}
-            {/*<A/>*/}
 
-            <Chart type={'line'}/>
-            {/*<ScatterChart/>*/}
-        </div>
-    );
+    </AppPageLayout>)
 }
 
 export default App;
-
-// id, // 방금 커밋된 Profiler 트리의 "id"
-//     phase, // "mount" (트리가 방금 마운트가 된 경우) 혹은 "update"(트리가 리렌더링된 경우)
-//     actualDuration, // 커밋된 업데이트를 렌더링하는데 걸린 시간
-//     baseDuration, // 메모이제이션 없이 하위 트리 전체를 렌더링하는데 걸리는 예상시간
-//     startTime, // React가 언제 해당 업데이트를 렌더링하기 시작했는지
-//     commitTime, // React가 해당 업데이트를 언제 커밋했는지
-//     interactions // 이 업데이트에 해당하는 상호작용들의 집합
