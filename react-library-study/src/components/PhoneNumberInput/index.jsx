ae5ad5d049458be4090934a08016f95ab6a0fbae
jyh4479 <jyh.dev@gmail.com>
2023-09-27 23:43:33 +0900

    test rifm

--- react-library-study/src/components/PhoneNumberInput/index.jsx
+++ react-library-study/src/components/PhoneNumberInput/index.jsx
@@ -2,34 +2,27 @@ import React, {useState} from 'react';
 import {Rifm} from "rifm";
 
 const PhoneNumberInput = (props) => {
-    const [dateTime, setDateTime] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const formatPhoneNumber = (str) => {
+        const cleaned = str.replace(/\D/g, '');
+        const match = cleaned.match(/^(\d{0,3})(\d{0,4})(\d{0,4})$/);
+        if (match) {
+            return `${match[1]}-${match[2]}-${match[3]}`;
+        }
+        return '';
+    };
 
-    const handleDateTimeChange = (formattedValue) => {
-        setDateTime(formattedValue);
+    const handlePhoneNumberChange = (formattedValue) => {
+        setPhoneNumber(formattedValue);
     };
 
     return (
         <div>
-            <label>날짜 및 시간:</label>
+            <label>휴대폰 번호:</label>
             <Rifm
-                value={dateTime}
-                onChange={handleDateTimeChange}
-                format={(str) => {
-                    // 입력 값을 "YYYY-MM-DD HH:mm" 형식에 맞게 변환
-                    const cleaned = str.replace(/[^\d:-]/g, ''); // 숫자, 콜론(:), 대시(-)만 허용
-                    const match = cleaned.match(/^(\d{0,4})(:?)(\d{0,2})(:?)(\d{0,2})(:?)(\d{0,2})?(:?)(\d{0,2})?$/);
-
-                    if (match) {
-                        const year = match[1] || '';
-                        const month = match[3] || '';
-                        const day = match[5] || '';
-                        const hour = match[7] || '';
-                        const minute = match[9] || '';
-
-                        return `${year}-${month}-${day} ${hour}:${minute}`;
-                    }
-                    return '';
-                }}
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+                format={formatPhoneNumber}
             >
                 {({value, onChange}) => (
                     <input
@@ -43,5 +36,52 @@ const PhoneNumberInput = (props) => {
     );
 }
 
-
-export default PhoneNumberInput;
+// export default PhoneNumberInput;
+//
+// import React, { useState } from 'react';
+// import { Rifm } from 'rifm';
+//
+// function PhoneNumberInput() {
+//     // 휴대폰 번호 상태 초기화
+//     const [phoneNumber, setPhoneNumber] = useState('');
+//
+//     // 'Rifm'의 'onChange' 핸들러
+//     const handlePhoneNumberChange = (formattedValue) => {
+//         // 형식화된 값을 상태에 저장
+//         setPhoneNumber(formattedValue);
+//     };
+//
+//     return (
+//         <div>
+//             <label>휴대폰 번호:</label>
+//             <Rifm
+//                 value={phoneNumber}
+//                 onChange={handlePhoneNumberChange}
+//                 format={(str) => {
+//                     // 입력 값을 "010-1234-5678" 형식에 맞게 변환
+//                     const cleaned = str.replace(/[^\d-]/g, ''); // 숫자와 대시(-)만 허용
+//                     const match = cleaned.match(/^(\d{0,3})(-?)(\d{0,4})(-?)(\d{0,4})?$/);
+//
+//                     if (match) {
+//                         const firstPart = match[1] || '';
+//                         const secondPart = match[3] || '';
+//                         const thirdPart = match[5] || '';
+//
+//                         return `${firstPart}${match[2]}${secondPart}${match[4]}${thirdPart}`;
+//                     }
+//                     return '';
+//                 }}
+//             >
+//                 {({ value, onChange }) => (
+//                     <input
+//                         type="text"
+//                         value={value}
+//                         onChange={(e) => onChange(e.target.value)}
+//                     />
+//                 )}
+//             </Rifm>
+//         </div>
+//     );
+// }
+//
+// export default PhoneNumberInput;
