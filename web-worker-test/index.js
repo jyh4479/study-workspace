7c1be08f691cb6ccb53b48f32074bb4bb9fc5f6f
jyh4479 <jyh.dev@gmail.com>
2023-12-16 12:55:23 +0900

    study: web worker

--- web-worker-test/index.js
+++ web-worker-test/index.js
@@ -1,38 +1,24 @@
 const workerButton = document.getElementById("worker-button");
 const workerStatus = document.getElementById("worker-status");
 
-const myWorker = new Worker("worker.js");
 let myWorkerStatus = "ready";
+let count = 0;
 
 const onClickWorker = () => {
-    myWorker.postMessage("hi worker!");
+    const myWorker = new Worker("worker.js");
+    myWorker.postMessage(count++);
     myWorkerStatus = "run";
     workerStatus.textContent = myWorkerStatus;
-}
 
-myWorker.onmessage = (e) => {
-    console.log(e.data);
-    myWorkerStatus = "ready";
-    workerStatus.textContent = myWorkerStatus;
+    myWorker.onmessage = (e) => {
+        console.log(e.data);
+        myWorkerStatus = "ready";
+        workerStatus.textContent = myWorkerStatus;
+    }
 }
 
 workerButton.addEventListener("click", onClickWorker);
 
-
-// if (window.Worker) {
-//     const myWorker = new Worker("worker.js");
-//
-//     [first, second].forEach(input => {
-//         input.onchange = function() {
-//             myWorker.postMessage([first.value, second.value]);
-//             console.log('Message posted to worker');
-//         }
-//     })
-//
-//     myWorker.onmessage = function(e) {
-//         result.textContent = e.data;
-//         console.log('Message received from worker');
-//     }
-// } else {
-//     console.log('Your browser doesn\'t support web workers.');
-// }
+setInterval(() => {
+    console.log("normal console");
+}, 500);
--- web-worker-test/worker.js
+++ web-worker-test/worker.js
@@ -1,18 +1,5 @@
 onmessage = (e) => {
     console.log('Worker: Message received from main script');
-    console.log(e.data);
-    for (let i = 0; i < 1000000000; i++) ;
-    postMessage("bye worker!");
+    for (let i = 0; i < 10000000000; i++) ;
+    postMessage(`bye worker! ${e.data}`);
 }
-
-// onmessage = function(e) {
-//     console.log('Worker: Message received from main script');
-//     const result = e.data[0] * e.data[1];
-//     if (isNaN(result)) {
-//         postMessage('Please write two numbers');
-//     } else {
-//         const workerResult = 'Result: ' + result;
-//         console.log('Worker: Posting message back to main script');
-//         postMessage(workerResult);
-//     }
-// }
