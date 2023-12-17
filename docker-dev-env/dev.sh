f2c32a876f05332f4952dc18120fa36160f36486
jyh4479 <jyh.dev@gmail.com>
2023-12-17 15:20:52 +0900

    study: docker env

--- docker-dev-env/dev.sh
+++ docker-dev-env/dev.sh
@@ -2,4 +2,4 @@
 
 docker build -t docker-dev-env-image .
 
-docker run -d -p 80:5173 --name docker-dev-env-container docker-dev-env-image
+docker run -v $(pwd)/src:/app/src -d -p 80:5173 --name docker-dev-env-container docker-dev-env-image
